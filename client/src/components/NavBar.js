ca939ab254dd40d203be4bce42a92d555c1db5ab
oljakostiv <oljakostivv@gmail.com>
2022-01-23 22:32:24 +0200

    add auth

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,37 +1,42 @@
 import React, {useContext} from 'react';
-import {Context} from "../index";
+import {observer} from "mobx-react-lite";
 import {Container, Nav, Navbar, Button} from "react-bootstrap";
 import {NavLink} from "react-router-dom";
+import {Context} from "../index";
 import {SHOP_ROUTE} from "../utils/consts";
-import {observer} from "mobx-react-lite";
 
 const NavBar = observer(() => {
-    const { user } = useContext(Context);
+    const {user} = useContext(Context);
+
     return (
         <>
             <Navbar bg="light" variant="light">
                 <Container>
                     <NavLink
-                        style={{ textDecoration: 'none', color: 'grey', fontWeight: 'bold', fontSize: '20px' }}
-                        to={ SHOP_ROUTE }>
+                        style={{textDecoration: 'none', color: 'grey', fontWeight: 'bold', fontSize: '20px'}}
+                        to={ SHOP_ROUTE }
+                    >
                         TechStore
                     </NavLink>
 
-                    { user.isAuth ?
+                    {user.isAuth ?
                         <Nav className="ml-auto">
-                            <Button variant={ 'outline-secondary' }>Log In</Button>
-                            <Button variant={ 'outline-secondary' }
-                                    style={{ marginLeft: '1em' }}>
+                            <Button variant={'outline-secondary'}>Log In</Button>
+                            <Button variant={'outline-secondary'}
+                                    style={{marginLeft: 20}}
+                            >
                                 Admin panel
                             </Button>
                         </Nav>
                         :
                         <Nav className="ml-auto">
-                            <Button variant={ 'outline-secondary' }
-                                    onClick={ () => user.setIsAuth(true) }>Authorisation</Button>
+                            <Button variant={'outline-secondary'}
+                                    onClick={() => user.setIsAuth(true)}
+                            >
+                                Authorisation
+                            </Button>
                         </Nav>
                     }
-
                 </Container>
             </Navbar>
         </>
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,66 @@
 import React from 'react';
+import {Button, Card, Container, Form, Row} from "react-bootstrap";
+import {NavLink, useLocation} from "react-router-dom";
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
 
 const Auth = () => {
+    const location = useLocation();
+
+    const isLogin = location.pathname === LOGIN_ROUTE;
+
     return (
-        <div>
-            Auth
-        </div>
+        <Container
+            className='d-flex justify-content-center'
+            style={{marginTop: 200}}
+        >
+            <Card className='p-4' style={{width: 600}}>
+                <h2 className='m-auto'>{isLogin ? 'Authorization' : 'Registration'}</h2>
+
+                <Form className='d-flex flex-column'>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label">
+                            Email address
+                        </label>
+                        <input type="email" className="form-control"
+                               id="exampleInputEmail1" aria-describedby="emailHelp"
+                        ></input>
+                        <div id="emailHelp" className="form-text">
+                            We'll never share your email with anyone else.
+                        </div>
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputPassword1" className="form-label">
+                            Password
+                        </label>
+                        <input type="password" className="form-control" id="exampleInputPassword1"></input>
+                    </div>
+
+                    <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="exampleCheck1"></input>
+                        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                    </div>
+
+                    <Row className='d-flex justify-content-around mt-1'>
+                        {isLogin ?
+                            <div className='w-50'>
+                                No account? <NavLink to={ REGISTRATION_ROUTE }>Registering</NavLink>
+                            </div>
+                            :
+                            <div className='w-50'>
+                                Account exist? <NavLink to={ LOGIN_ROUTE }>Log in</NavLink>
+                            </div>
+                        }
+                        <Button
+                            className='w-25'
+                            variant={'outline-success'}
+                        >
+                            Submit
+                        </Button>
+                    </Row>
+                </Form>
+            </Card>
+        </Container>
     );
 };
 
